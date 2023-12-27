3a299a0731f93dbcbcfe47f0e5052fc0a5c0a72d
Matias Canton <matiasacanton@gmail.com>
2023-12-27 16:18:11 -0300

    test nuevo

--- api/sendMails.js
+++ api/sendMails.js
@@ -1,5 +1,7 @@
 // api/sendMails.js
 
+const fs = require('fs');
+
 const sendMails = async () => {
     try {
         // Lógica de envío de correos electrónicos aquí
@@ -7,17 +9,16 @@ const sendMails = async () => {
         // Mensaje para imprimir en la consola
         console.log('Envío de correos electrónicos realizado con éxito.');
 
+        // Mensaje para escribir en un archivo
+        fs.appendFileSync('output.log', 'Envío de correos electrónicos realizado con éxito.\n');
+
     } catch (error) {
         // Manejar errores aquí
 
-        // Imprimir errores en la consola
+        // Imprimir errores en la consola y en el archivo
         console.error('Error al enviar correos electrónicos:', error);
+        fs.appendFileSync('output.log', `Error al enviar correos electrónicos: ${error}\n`);
     }
 };
 
 module.exports = sendMails;
-
-// Añadir la ejecución directa si el archivo es ejecutado directamente
-if (require.main === module) {
-    sendMails();
-} 
